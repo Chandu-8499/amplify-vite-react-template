f27340c69764e2aa679b81ac8b495354b17b5334
Chandu-8499 <gana.chandu999@gmail.com>
2024-09-17 09:28:47 +0530

    main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,34 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// import { Amplify } from "aws-amplify";
+import { Amplify } from "aws-amplify"
 
-// import outputs from "../amplify_outputs.json";
-
-// Amplify.configure(outputs);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "<your-cognito-user-pool-id>",
+      userPoolClientId: "<your-cognito-user-pool-client-id>",
+      identityPoolId: "<your-cognito-identity-pool-id>",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
