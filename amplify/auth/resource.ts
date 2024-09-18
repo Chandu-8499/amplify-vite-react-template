ba95642becbcc2c44a4ae8d3c4e7d4e90b47c37a
Chandu-8499 <gana.chandu999@gmail.com>
2024-09-18 09:38:30 +0530

    app

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,14 @@
-import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema, defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+})
 
 const schema = a.schema({
   Product: a.model({
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
-import type { FormEvent } from "react"
-import { Amplify } from "aws-amplify"
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "../amplify_outputs.json";
 
-
-
-import { signUp } from "aws-amplify/auth"
-import outputs from "../amplify_outputs.json"
-
-
-Amplify.configure(outputs)
-
-interface SignUpFormElements extends HTMLFormControlsCollection {
-  email: HTMLInputElement
-  password: HTMLInputElement
-}
-
-interface SignUpForm extends HTMLFormElement {
-  readonly elements: SignUpFormElements
-}
+Amplify.configure(outputs);
 
 export default function App() {
-  async function handleSubmit(event: FormEvent<SignUpForm>) {
-    event.preventDefault()
-    const form = event.currentTarget
-    // ... validate inputs
-    await signUp({
-      username: form.elements.email.value,
-      password: form.elements.password.value,
-    })
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" />
-      <input type="submit" />
-    </form>
-  )
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
-
